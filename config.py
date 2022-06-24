a42382f0731c3e12b27c98f5e602add8f62a9375
vherrmann <viktor.herrmann@hhi.fraunhofer.de>
2022-06-24 15:25:29 +0200

    added configs

--- config.py
+++ config.py
@@ -3,14 +3,18 @@
 # number of test images
 TESTS = 1
 SAMPLE_STYLE = 'conditional'
-WIN_SIZE = 10 
-OVERLAPPING = 'stride' # 'None' 'full'
-STRIDE      = 5
+WIN_SIZE = 20 
+OVERLAPPING = 'None' #'stride' 'None' 'full'
+STRIDE      = 5 # only for stride
 NUM_SAMPLES = 10
 PADDING_SIZE = 2
+MODEL = 'resnet50' # 'mobilenet_v2'
+
+SOFTMAX = True # does not work
 
 SHOW = False   # show image before processing
-IMG_IDX = 1
+IMG_IDX = 9
 
+UPDATE = True
 
-BATCH_SIZE = 128
+BATCH_SIZE = 64
