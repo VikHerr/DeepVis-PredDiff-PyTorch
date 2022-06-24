421ef9d9a4add7de9aa07c6bfa9eed86a19625ad
vherrmann <viktor.herrmann@hhi.fraunhofer.de>
2022-06-24 15:25:42 +0200

    added more models

--- main.py
+++ main.py
@@ -21,7 +21,13 @@ def infoResNet(show=False,mVersion='resnet50'):
     '''
 
     #model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
-    model = tv.models.resnet50(pretrained=True)
+    if mVersion == 'resnet50':
+        model = tv.models.resnet50(pretrained=True)
+    elif mVersion == 'resnet18':
+        model = tv.models.resnet18(pretrained=True)
+    else:
+        assert False, 'invaled resnet version'
+
     model.eval()
     # define preprocess and disply corpping functions
     preprocess = tv.transforms.Compose([
@@ -41,9 +47,9 @@ def infoMobileNetV2(show=False):
     '''
     mobelNetV2 model info
     '''
-    # model = tv.models.mobilenet_v2(pretrained=True)
-    # if show: 
-    #     print('eval model: \n', model)
+    model = tv.models.mobilenet_v2(pretrained=True)
+    if show: 
+        print('eval model: \n', model)
 
     # model.eval()
 
@@ -61,6 +67,54 @@ def infoMobileNetV2(show=False):
 
     return {'model' : "moblienet_v2",'preprocess' : preprocess, 'display' : display}
 
+def infoAlexNet(show=False):
+    '''
+    mobelNetV2 model info
+    '''
+    model = tv.models.alexnet(pretrained=True, progress=True)
+
+    if show: 
+        print('eval model: \n', model)
+
+    # define preprocess and disply corpping functions
+    preprocess = tv.transforms.Compose([
+        tv.transforms.Resize(256),
+        tv.transforms.CenterCrop(224),
+        tv.transforms.ToTensor(),
+        tv.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])
+    
+    display = tv.transforms.Compose([
+        tv.transforms.Resize(256),
+        tv.transforms.CenterCrop(224),
+        tv.transforms.ToTensor()])
+
+    return {'model' : "alexnet",'preprocess' : preprocess, 'display' : display}
+
+def infoVgg16(show=False):
+    '''
+    mobelNetV2 model info
+    '''
+    model = tv.models.vgg16(pretrained=True, progress=True)
+
+    if show: 
+        print('eval model: \n', model)
+
+    # define preprocess and disply corpping functions
+    preprocess = tv.transforms.Compose([
+        tv.transforms.Resize(256),
+        tv.transforms.CenterCrop(224),
+        tv.transforms.ToTensor(),
+        tv.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])
+    
+    display = tv.transforms.Compose([
+        tv.transforms.Resize(256),
+        tv.transforms.CenterCrop(224),
+        tv.transforms.ToTensor()])
+
+    return {'model' : "vgg16",'preprocess' : preprocess, 'display' : display}
+
+
+
 def test_prob(classifier, x):
     '''
     test predict function 
@@ -69,36 +123,69 @@ def test_prob(classifier, x):
     
     cat_prob = classifier.predict(x) 
 
-    print(cat_prob.shape)
+    print(np.max(cat_prob), np.min(cat_prob))
+
+def viewImage(model):
+
+    dataLoader = udl.DataLoader(model=model, path='./img/')
+    #dataLoader = udl.DataLoader(model=model)# , path='./img/')
+
+    test_size = TESTS
+    show      = True
+    X_test,X_test_im, X_filenames = dataLoader.get_imagenet_data(s_idx=IMG_IDX, b_size=test_size, set_size=1)
+
+    classifier = ucls.Classifier(model=model['model'], softmax=False)
+
+    classifier.softmax = False   
+    test_prob(classifier, X_test)
+    classifier.softmax = True
+    test_prob(classifier, X_test)
+
+
+    for test_idx in range(test_size):
+
+        if show:
+            plt.imshow(X_test_im[test_idx])
+            plt.show()
+
+        top_prob, top_catid = classifier.getTopCat(X_test[test_idx:test_idx+1])
+        y_pred_label = classifier.categories[top_catid]
+        print('ylable', y_pred_label,'idx %d prob %.3f' %(top_catid,top_prob))
 
 
 def experiment(model):
 
-    dataLoader = udl.DataLoader(model=model)
+    dataLoader = udl.DataLoader(model=model, path='./img/')
 
     test_size = TESTS
     show      = SHOW
-    X_test,X_test_im, X_filenames = dataLoader.get_imagenet_data(s_idx=IMG_IDX, b_size=test_size)
+    softmax = SOFTMAX
+    X_test,X_test_im, X_filenames = dataLoader.get_imagenet_data(s_idx=IMG_IDX, b_size=test_size, set_size=8)
 
-    print(X_test.shape, X_filenames)
     path_results = './results/'
     if not os.path.exists(path_results):
         os.makedirs(path_results)  
 
-    classifier = ucls.Classifier(model=model['model'])
+    classifier = ucls.Classifier(model=model['model'], softmax=softmax)
     # Test: get prob of class from 
     #test_prob(classifier,X_test)
 
 
     for test_idx in range(test_size):
 
-        top_prob, top_catid = classifier.getTopCat(X_test[test_idx:test_idx+1])
+        x_test = X_test[test_idx]
+        x_test_im = X_test_im[test_idx]
+
+        top_prob, top_catid = classifier.getTopCat(x_test[np.newaxis])
+
         y_pred_label = classifier.categories[top_catid]
         print('ylable', y_pred_label,'idx %d prob %.3f' %(top_catid,top_prob))
 
+
+
         # show image
         if show:
-            plt.imshow(X_test_im[test_idx])
+            plt.imshow(x_test_im)
             plt.show()
 
         start_time = time.time()
@@ -110,26 +197,34 @@ def experiment(model):
             print('Unknown test type')
             return 
 
-        pda = PredDiffAnalyser(X_test[0:1], classifier, sampler, num_samples=NUM_SAMPLES, batch_size=BATCH_SIZE)
+        pda = PredDiffAnalyser(x_test[np.newaxis], classifier, sampler, num_samples=NUM_SAMPLES, batch_size=BATCH_SIZE)
         pred_diff = pda.get_rel_vect(win_size=WIN_SIZE, overlap=OVERLAPPING)
 
         # for now
-        sensMap = np.zeros(X_test_im[test_idx].shape)
+        sensMap = np.zeros(x_test_im.shape)
 
-        # plot and save the results
-        uv.plot_results(X_test[test_idx], X_test_im[test_idx], sensMap, pred_diff[0], classifier.categories, top_catid, save_path)
-        np.savez(save_path, *pred_diff)
+        print('diff shape', pred_diff[0].shape)
+
         print ("--- Total computation took {:.4f} minutes ---".format((time.time() - start_time)/60))
+        # plot and save the results
+        uv.plot_results(x_test, x_test_im, sensMap, pred_diff[0], classifier.categories, top_catid, save_path)
+        uv.plot_results(x_test, x_test_im, sensMap, pred_diff[0], classifier.categories, 385, save_path)
+        uv.plot_results(x_test, x_test_im, sensMap, pred_diff[0], classifier.categories, 386, save_path)
+        #np.savez(save_path, *pred_diff)
+        print('result:', save_path)
     
+
 
 
 def main():
 
-    #model = infoMobileNetV2()
+    model = infoMobileNetV2(show=False)
     # resnet versions: 18,34,50,101,152
-    model = infoResNet(mVersion='resnet18')
+    # model = infoResNet(mVersion='resnet50')
+    # model = infoAlexNet()
+    # model = infoVgg16()
     experiment(model)
-
+    # viewImage(model)
 
 
 if __name__=='__main__':
